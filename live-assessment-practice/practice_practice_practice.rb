30265c33fe96b618e8d45bdd2a149c54a1b25fab
Scott Hoecker <hoeckers@gmail.com>
2021-11-29 18:46:50 -0600

    Practice practice practice

---
+++ live-assessment-practice/practice_practice_practice.rb
@@ -0,0 +1,12 @@
+=begin
+
+There is a truck to be loaded with boxes. Each box has a length and width.
+
+Return the width of the arrangement of boxes with the minimum length.
+
+Input: A group of boxes with heights and widths
+Output: The length of the arrangement of boxes with the minimum total length
+
+
+
+=end
